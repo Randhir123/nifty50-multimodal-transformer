3a28333d71c1b38ab748603c2f67c16fe3711d26
Randhir Singh <randhirkumars@yahoo.com>
2026-04-05 07:11:26 +0530

    Implement Milestone 6 multi-source company text branch

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,10 +2,13 @@
 
 from src.models.image_transformer import ImageTransformer, ImageTransformerConfig
 from src.models.tabular_transformer import TabularTransformer, TabularTransformerConfig
+from src.models.text import CompanyTextTransformer, CompanyTextTransformerConfig
 
 __all__ = [
     "TabularTransformer",
     "TabularTransformerConfig",
     "ImageTransformer",
     "ImageTransformerConfig",
+    "CompanyTextTransformer",
+    "CompanyTextTransformerConfig",
 ]
